5613e9176d6e2a5146a70c68e3cc367230c1ad60
John Weis <weis.john@gmail.com>
2011-08-25 19:30:59 -0400

    adding the tweeze project to the repo

---
+++ tweeze/js/jquery.tweeze.js
@@ -0,0 +1,26 @@
+/*
+
+
+*/
+(function($) {
+	
+	$.fn.tweeze = function(options) {  
+		  
+		var defaults = {
+			
+			username: 'weisjohn',
+			
+		};
+		var options = $.extend(defaults, options);
+		
+		// TODO: write some functions to grab results from twitter
+		
+	    return this.each(function() {  
+	  
+	    	
+	    	
+	    });  
+
+	};  
+	
+})(jQuery);
