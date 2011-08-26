b272dfe640646bb7c6e66fda0ff7a94803d02208
John Weis <weis.john@gmail.com>
2011-08-26 18:40:36 -0400

    v0.1 - we now can pull and display tweets from the Search API

--- tweeze/js/jquery.tweeze.js
+++ tweeze/js/jquery.tweeze.js
@@ -20,8 +20,9 @@
 
             linkify			: true,
             show_senders	: false,
+			show_faces		: false,		// does nothing yet
             usernames : {
-            	'from' : ['weisjohn'],
+            	'from' : [],
             	'to'   : []
         	}
 
@@ -64,56 +65,58 @@
         
         function _build_request_url() {
         	
-        	var request_URI = settings.base_url;
+        	var query_string = "";
         	
         	// if from or to are < 3, then the results are only filtered after the fact
         	// as the Search API has simplicity requirements
         	
-        	// this is also problably one of the driest pieces of code i have written in a long time... 
+        	// this is also probably one of the DRYest pieces of code i have written in a long time... 
+			// loop through and build the @from and @to params
         	$.each(settings.usernames, function(k, set) {
-        		
         		if (set.length < 3) {
-        			
         			$.each(set , function(i, name){
-    	        		
-    	        		if (i > 0 ) {
-    	        			request_URI += "OR+";
-    					}
-    	        		request_URI += k + ":" + name + "+";
-    	        		
+    	        		if (i > 0 ) { query_string += "OR+"; }
+    	        		query_string += k + ":" + name + "+";
     	        	});
         		}
-        		
         	});
         	
         	
         	if (settings.usernames.length < 3){
-        		
 	        	$.each( settings.from_usernames, function(i, val){
-	        		
 	        		if (i > 0 ) {
-	        			request_URI += "OR+";
+	        			query_string += "OR+";
 					}
-	        		request_URI += "from:" + val + "+";
-	        		
+	        		query_string += "from:" + val + "+";
 	        	});
         	}
         	
+        	if (typeof settings.hashtag_filter == 'string') {
+				
+				query_string += "#" + settings.hashtag_filter;
+			}
         	
-        	
-        	console.log('request_URI: ' + request_URI);
-        	return escape(request_URI); 
+        	console.log(settings.base_url + escape(query_string));
+        	return settings.base_url + escape(query_string); 
         }
         
-
-        function contains(a, obj) {
-            for (var i = 0; i < a.length; i++) {
-                if (a[i] === obj) {
-                    return true;
-                }
-            }
-            return false;
-        }
+		// in logic filter to avoid the complexity requirements from the Twitter Search API
+		function _filter_from(tweet) {
+			
+			var matches = false;
+			
+			if (settings.usernames['from'].length > 0) {
+				$.each(settings.usernames['from'], function(i, name) {
+					if (tweet.from_user.toLowerCase() == name.toLowerCase()) {
+						matches = true;	
+					}
+        		});
+			} else {
+				matches = true;
+			}
+			
+			return matches;
+		}
 
 
         // TODO: create functions such as update and destory - http://docs.jquery.com/Plugins/Authoring#Events
@@ -125,9 +128,87 @@
             }
             
             
-            _build_request_url();
-            // TODO: eventually build in the ability to register and just update, keeping a copy of the current tweets
+            var request_url = _build_request_url();
             
+			
+			// TODO: eventually build in the ability to register and just update, keeping a copy of the current tweets
+            
+			// TODO: make the request and render the DOM
+			
+			// capture a reference for later attaching
+			var _wrapper = $(this).addClass('tweeze');
+			
+			$.ajax({
+				url : request_url,
+				dataType : 'jsonp',
+				statusCode : {
+					404 : function() { console.log('file not found') },
+					420 : function() { console.log('increase the chill') },  // TODO: do something with this 
+				},
+				success : function(data) {
+					
+					// when re-writing for the caching layer, pull out the tweets into local storage,
+					// build / subscribe a jQuery custom event to listen to changes to localStorage
+					// 
+					
+					if (data['results']) {
+						
+						$.each(data['results'], function(i, tweet) {
+							
+							
+							// TODO: revisit this and remove if count is null
+							if (i < settings.count) {
+								
+								if (_filter_from(tweet)) {
+									
+									console.log('tweet ');
+									console.dir(tweet);
+									
+									// build and attach the tweet to the list
+									var t = $("<li></li>");
+									
+									// add the sender to the li
+									if (settings.show_senders) {
+										$("<a></a>")
+											.addClass('user')
+											.attr('target', '_blank')
+											.attr('href', 'http://twitter.com/' + tweet.from_user)
+											.html(tweet.from_user)
+											.appendTo(t);
+									}
+									
+									// add the text to the li
+									$("<span></span>")
+										.addClass('text')
+										.html( _linkify(tweet.text) )
+										.appendTo(t);
+										
+									// add the time to the li
+									$("<a></a>")
+										.addClass('time')
+										.attr('target', '_blank')
+										.attr('href', 'http://twitter.com/' + tweet.from_user + '/statuses/' + tweet.id_str)
+										.html( _prettyDate(tweet.created_at) )
+										.appendTo(t);
+										
+									t.appendTo(_wrapper);
+									
+								}
+								
+								
+								// 
+							
+							}
+							
+						});
+						
+					}
+					
+				}
+			});
+			
+			
+			
             
             //// we already have a copy of this - http://docs.jquery.com/Plugins/Authoring#Context
         });
