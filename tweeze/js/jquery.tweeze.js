decf49c2cdbfd4de5bcdb7455f6cc23d57506c04
John Weis <weis.john@gmail.com>
2011-08-31 16:14:39 -0400

    adding the 'requests per page' functionality to tweeze requesting scheme

--- tweeze/js/jquery.tweeze.js
+++ tweeze/js/jquery.tweeze.js
@@ -16,13 +16,16 @@
 
         var settings = {
         	base_url		: "http://search.twitter.com/search.json?q=",
-            count			: '10',
+            count			: '15',
             hashtag_filter	: null,
 
             linkify			: true,
+            max_attempts	: 3,
+            min_size		: 0,
             show_senders	: false,
 			show_faces		: false,		// does nothing yet
 			search_term		: null,
+			rpp				: null,
             usernames : {
             	'from' : [],
             	'to'   : []
@@ -31,11 +34,22 @@
         	on_complete : null
 
         };
+        
+        var _wrapper = null;
+        
+        var attempts = 0;
+        
+        var data_collection = {
+        	
+        	results : []	
+        	
+        };
+        
 
         // courtesy of Jeremy Parrish (rrish.org)
         function _linkify(text) {
 
-            return text.replace(/(https?:\/\/\S+)/gi, '<a target="_blank" href="$1">$1</a>').replace(/(^|\s)@(\w+)/g, '$1<a target="_blank" href="http://twitter.com/$2">@$2</a>').replace(/(^|\s)#(\w+)/g, '#<a target="_blank" href="http://search.twitter.com/search?q=%23$2">$1$2</a>');
+            return text.replace(/(https?:\/\/\S+)/gi, '<a target="_blank" href="$1">$1</a>').replace(/(^|\s)@(\w+)/g, '$1<a target="_blank" href="http://twitter.com/$2">@$2</a>').replace(/(^|\s)#(\w+)/g, ' <a target="_blank" href="http://search.twitter.com/search?q=%23$2">#$2</a>');
         };
 
         /* 
@@ -69,6 +83,7 @@
         
         function _build_request_url() {
         	
+        	var url = "";
         	var query_string = "";
         	
         	// if from or to are < 3, then the results are only filtered after the fact
@@ -101,11 +116,20 @@
 			}
 			
 			if (typeof settings.search_term == 'string') {
-				query_string += settings.search_term;
+				query_string += settings.search_term + '+';
 			}
+			
+			
         	
-        	return settings.base_url + escape(query_string); 
+			url = settings.base_url + escape(query_string);
+			
+			if (typeof settings.rpp == 'number') {
+				url += '&rpp=' + settings.rpp;
+			}
+			
+        	return url; 
         }
+        
         
 		// in logic filter to avoid the complexity requirements from the Twitter Search API
 		function _filter_from(tweet) {
@@ -124,7 +148,131 @@
 			
 			return matches;
 		}
+		
+		
+		function _push_tweet(tweet) {
+			
+			var duplicate = false;
+			$.each(data_collection.results, function(i, t){
+				
+				if (tweet.id_str == t.id_str) {
+					duplicate = true;
+				}
 
+			});
+			
+			if (!duplicate) {
+				data_collection.results.push(tweet);	
+			}
+			
+		}
+
+		// this retrieves the data from the Twitter Search API 
+		function _retrieve_data(search_string) {
+			
+			attempts++;
+			
+			$.ajax({
+				url : search_string,
+				dataType : 'jsonp',
+				statusCode : {
+					404 : function() { console.log('file not found'); },
+					420 : function() { console.log('increase the chill'); }  // TODO: do something with this 
+				},
+				success : function(data) {
+					
+					
+					if (data['results']) {
+					
+						console.dir(data['results']);
+						
+						$.each(data['results'], function(i, tweet) {
+						
+							// if this tweet is in the list of from users,
+							// or the list of from users isn't specified
+							// then we'll add it to the collection
+							if (_filter_from(tweet)) {
+								
+								// TODO: make sure we're not duplicating the tweet in the list
+								_push_tweet(tweet);
+							}
+							
+						});
+						
+						// if we still need more, let's call again, going further back in time
+						if ((data_collection.results.length < settings.min_size) && (attempts < settings.max_attempts)) {
+					
+							// TODO: go grab more tweets
+							console.log('i need more tweets!');
+							_retrieve_data(search_string);
+							
+						} else {
+							
+							// if we have an adequate # of tweets, let's render
+							_render_data();
+						}
+					
+					} else {
+						
+						// for some reason, the response from twitter didn't have any ['results']
+						// so let's call render just in case
+						_render_data();
+						
+					}
+					
+				}
+			});
+		}
+		
+		// this handles the DOM manipulation
+		function _render_data() {
+			
+			$.each(data_collection.results, function(i, tweet) {
+				
+				// only show the number that we need to see
+				if (i < settings.count) {
+					
+					// build and attach the tweet to the list
+					var t = $("<li></li>");
+					
+					// add the sender to the li
+					if (settings.show_senders) {
+						$("<a></a>")
+							.addClass('user')
+							.attr('target', '_blank')
+							.attr('href', 'http://twitter.com/' + tweet.from_user)
+							.html(tweet.from_user)
+							.appendTo(t);
+					}
+					
+					// add the text to the li
+					$("<span></span>")
+						.addClass('text')
+						.html( _linkify(tweet.text) )
+						.appendTo(t);
+						
+					// add the time to the li
+					$("<a></a>")
+						.addClass('time')
+						.attr('target', '_blank')
+						.attr('href', 'http://twitter.com/' + tweet.from_user + '/statuses/' + tweet.id_str)
+						.html( _prettyDate(tweet.created_at) )
+						.appendTo(t);
+						
+					t.appendTo(_wrapper);
+					
+				}
+				
+			});
+			
+			
+			if (typeof settings.on_complete == 'function') {
+				
+				settings.on_complete();
+			}
+			
+		}
+		
 
         // TODO: create functions such as update and destory - http://docs.jquery.com/Plugins/Authoring#Events
         //// returning a function with the .each maintains chainability - http://docs.jquery.com/Plugins/Authoring#Maintaining_Chainability 
@@ -133,93 +281,16 @@
             if (options) {
                 $.extend(settings, options);
             }
-            
-            
-            var request_url = _build_request_url();
-            
-			
+                        
 			// TODO: eventually build in the ability to register and just update, keeping a copy of the current tweets
-            
-			// TODO: make the request and render the DOM
 			
-			// capture a reference for later attaching
-			var _wrapper = $(this).addClass('tweeze');
+			// set the reference for later 
+			_wrapper = $(this).addClass('tweeze');
 			
-			$.ajax({
-				url : request_url,
-				dataType : 'jsonp',
-				statusCode : {
-					404 : function() { console.log('file not found') },
-					420 : function() { console.log('increase the chill') }  // TODO: do something with this 
-				},
-				success : function(data) {
-					
-					// when re-writing for the caching layer, pull out the tweets into local storage,
-					// build / subscribe a jQuery custom event to listen to changes to localStorage
-					// 
-					
-					if (data['results']) {
-						
-						$.each(data['results'], function(i, tweet) {
-							
-							
-							// TODO: revisit this and remove if count is null
-							if (i < settings.count) {
-								
-								if (_filter_from(tweet)) {
+			// grab the data with the default request URL
+				// once the data has been retrieved, the retriever calls the renderer
+			_retrieve_data(_build_request_url());
 
-									
-									// build and attach the tweet to the list
-									var t = $("<li></li>");
-									
-									// add the sender to the li
-									if (settings.show_senders) {
-										$("<a></a>")
-											.addClass('user')
-											.attr('target', '_blank')
-											.attr('href', 'http://twitter.com/' + tweet.from_user)
-											.html(tweet.from_user)
-											.appendTo(t);
-									}
-									
-									// add the text to the li
-									$("<span></span>")
-										.addClass('text')
-										.html( _linkify(tweet.text) )
-										.appendTo(t);
-										
-									// add the time to the li
-									$("<a></a>")
-										.addClass('time')
-										.attr('target', '_blank')
-										.attr('href', 'http://twitter.com/' + tweet.from_user + '/statuses/' + tweet.id_str)
-										.html( _prettyDate(tweet.created_at) )
-										.appendTo(t);
-										
-									t.appendTo(_wrapper);
-									
-								}
-								
-								
-								// 
-							
-							}
-							
-						});
-						
-					}
-					
-					if (typeof settings.on_complete == 'function') {
-						
-						settings.on_complete();
-					}
-					
-				}
-			});
-			
-			
-			
-            
             //// we already have a copy of this - http://docs.jquery.com/Plugins/Authoring#Context
         });
 
