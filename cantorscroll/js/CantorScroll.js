be1c0b3d79034bdfcbbacc41fb7fd168ea3616c9
John Weis <weis.john@gmail.com>
2011-10-20 02:31:13 -0400

    i can now find the number of elements in the grid that are visible

--- cantorscroll/js/CantorScroll.js
+++ cantorscroll/js/CantorScroll.js
@@ -33,7 +33,7 @@
 			done : false, // For when it goes all the way through the archive.
 			paused : false,
 			
-			page_cursor : 1,
+			page_cursor : 0,
 			sort_type : "",
 			category : ""
 		},
@@ -103,21 +103,50 @@
             }
         },
 		
+		
 		_near_last_retrieved_page : function canscr_near_last_retrieved_page() {
 			
 			var opts = this.options;
-			
+			var state = opts.view_state;
 			
 			
 			// TODO: if the trigger position is beyond the trigger point, trigger
 				
-				// TODO: calculate trigger position
+				// calculate trigger position
 				var trigger_position = $(window).scrollTop() + $(window).height() + opts.buffer_px;
 					
-				// TODO: calculate trigger point
-			
-			
-			
+				// calculate page_cursor value
+				var children_visible = 0;
+					
+					// find the number of elements that have tops above the trigger_position
+					this.element.children().each(function() { 
+					
+						if ( $(this).offset()['top'] < trigger_position ) {
+							
+							$(this).css('border', '2px solid yellow');
+							children_visible++;
+						}
+					});
+				
+					this._debug('children visible:', children_visible);
+				
+					// opts.page_visited[state.sort_type][state.category]
+					this._debug('current page:', state.page_cursor);
+				
+					var visited_current_page = false;
+					for (var i = 0; i < opts.pages_visited[state.sort_type][state.category].length; i++) {
+						if ( state.page_cursor == opts.pages_visited[state.sort_type][state.category][i] ) 
+							visited_current_page = true;	
+					}
+				
+				this._debug('current page visited?', visited_current_page);
+				this._debug('opts.pages_visited[state.sort_type][state.category]', opts.pages_visited[state.sort_type][state.category]);
+				
+				// if we haven't visited this page, we need to retrieve elements
+				// TODO: do this after the ajax call to retrieve more elements
+				// set the current page_visited to be the page we're on
+				// TODO: uncomment this once the page_cursor value is calculated correctly
+				// opts.pages_visited[state.sort_type][state.category] = [state.page_cursor];
 			
 			
 			// logic from the infinite scroll... 
@@ -143,6 +172,25 @@
          * public methods
          * -------------- 
 		 */
+		bind : function canscr_bind() {
+			this._binding('bind');
+		},
+		
+		filter : function canscr_filter(c) {
+			
+			var opts = this.options,
+				state = opts.view_state;
+			
+			// make sure this is a valid category		
+			for (var i = 0; i < opts.categories.length; i++) {
+				
+				if (c == opts.categories[i]) {
+					this._debug('changing category from:', state.category, 'to:', c);
+					
+					state.category = c;
+				}
+			}
+		},
 		
 		
 		// based on current categories / sorts, retrieve the next page 
@@ -156,6 +204,7 @@
 			// TODO: add dummy foo objects?
 			
 		},
+		
 		
 		// Check to see next page is needed
         scroll : function canscr_scroll() {
@@ -174,7 +223,7 @@
         },
 		
 		// change
-		sort : function(s) {
+		sort : function canscr_sort(s) {
 			
 			var opts = this.options,
 				state = opts.view_state;
@@ -190,20 +239,8 @@
 			}
 		},
 	
-		filter : function(c) {
-			
-			var opts = this.options,
-				state = opts.view_state;
-			
-			// make sure this is a valid category		
-			for (var i = 0; i < opts.categories.length; i++) {
-				
-				if (c == opts.categories[i]) {
-					this._debug('changing category from:', state.category, 'to:', c);
-					
-					state.category = c;
-				}
-			}
+		unbind : function canscr_unbind() {
+			this._binding('unbind');
 		}
 		
 		
