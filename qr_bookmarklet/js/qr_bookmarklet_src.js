0d5e50fae917fa4cb060d59e8a8c8ce2bd037089
John Weis <weis.john@gmail.com>
2011-12-22 17:07:01 -0500

    adding new files to the github repo page

---
+++ qr_bookmarklet/js/qr_bookmarklet_src.js
@@ -0,0 +1,89 @@
+
+
+(function() {
+	
+	function load_script(src, test, on_asset_load){
+	
+		if (!test() ){
+			
+			var s = document.createElement('script');
+			s.setAttribute('src',src);
+			document.getElementsByTagName('body')[0].appendChild(s);
+
+			(function wait_timer() {
+
+				setTimeout(function(){
+
+					if (test()) {
+						on_asset_load();
+					} else {
+						wait_timer();
+					}
+
+				}, 10);
+
+			})();
+			
+		}
+
+	}
+	
+	load_script("http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js", function(){
+
+		return window.jQuery;
+		
+	}, function(){
+		
+		load_script("file:///Users/jweis/mysrc/widgets/qr_bookmarklet/js/jquery.qrcode.js/qrcode.js", function() {
+			
+			return window.QR8bitByte;
+			
+		}, function() {
+			
+			load_script("file:///Users/jweis/mysrc/widgets/qr_bookmarklet/js/jquery.qrcode.js/jquery.qrcode.js", function(){
+			
+				return $.fn.qrcode;
+				
+			}, function() {
+				
+				window.gencode = function() {
+					
+				$("<div></div>")
+					.appendTo("body")
+					.css({ 
+						position: "absolute", 
+						"top" : "0",
+						"border" : "20px solid #FFFFFF"
+					}).qrcode({
+						width: 280,
+						height: 280,
+						text : location.href,
+						correctLevel : QRErrorCorrectLevel.L
+					}).append(
+						$("<a></a>")
+							.html("close")
+							.attr("href", "#")
+							.css({ 
+								"background" : "#CCCCCC",
+								"display" : "block",
+								"textAlign" : "center",
+								"textDecoration" : "none",
+								"fontSize" : "24px",
+								"color" : "black",
+								"padding" : "6px"
+							}).on("click", function(){
+								$(this).parent().remove();
+							})
+					);
+				
+				}
+				
+				// gencode();
+				
+			});
+			
+		});
+		
+ 	});
+		
+})();
